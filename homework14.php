d9e0400c33da6042565feb6d91b240fba151a20a
yahont88 <yakovkrilov@gmail.com>
2019-07-20 22:12:47 +0300

    1part on lesson14 misswork

--- homework14.php
+++ homework14.php
@@ -29,74 +29,92 @@ echo '<br>';
 */
 
     //echo count($date);
+
+
     while ($date <= time()) {
       echo mt_rand(0, 528621663 );
+    if ($date) {
+      echo date ('d.m.Y H:i:s', $date);
     }
+  }
+  
+echo '<br>';
 
 echo '<br>';
-    show_date($date);
-function show_date ($date = []) {
-  if ($date) {
-    echo date ('d.m.Y H:i:s', $date);
+
+        show_date($date);
+    function show_date ($date = []) {
+      if ($date) {
+        echo date ('d.m.Y H:i:s', $date);
+      }
+
+    foreach ($date as $key => $dateshow) { 
+      echo date ('d.m.Y H:i:s',$dateshow);
+      echo '<br>';
+    }
   }
 
-  foreach ($date as $key => $dateshow) { 
-    echo date ('d.m.Y H:i:s',$dateshow);
-    echo '<br>';
-  }
-}
-    echo '<br>';
+echo '<br>';
 
-    echo '<br>';
+echo '<br>';
 
-//echo min($date);
-$mindate = min($date);
-  echo date('d.m.Y H:i:s', $mindate);
-    echo '<br>';
-  echo 'самая ранняя дата!';
+    //echo min($date);
+    $mindate = min($date);
+      echo date('d.m.Y H:i:s', $mindate);
+        echo '<br>';
+      echo 'самая ранняя дата!';
 
 echo '<br>';
 
 echo '<br>';
-//$sortdate = sort($date);// строчка sort($date) далее применена для получения списка, вывести его по принципу $mindate не получилось
-sort($date);
-//echo date('d.m.Y H:i:s', $sortdate);//почему-то вывести это списком не получилось 
-  var_dump($date);
-    echo '<br>';
-  echo 'отсортированы';
 
-    echo '<br>';
+    //$sortdate = sort($date);// строчка sort($date) далее применена для получения списка, вывести его по принципу $mindate не получилось
+    sort($date);
+    //echo date('d.m.Y H:i:s', $sortdate);//почему-то вывести это списком не получилось 
+      var_dump($date);
+        echo '<br>';
+      echo 'отсортированы';
 
-    echo '<br>';
+echo '<br>';
 
-$last_date = max($date);
-  echo date('d.m.Y H:i:s', $last_date);
-    echo '<br>';
-  echo 'самая поздняя дата!';
+echo '<br>';
 
-    echo '<br>';
+    $last_date = max($date);
+      echo date('d.m.Y H:i:s', $last_date);
+        echo '<br>';
+      echo 'самая поздняя дата!';
 
-    echo '<br>';
+echo '<br>';
 
-  date_default_timezone_set('America/New_York');
-  echo date('d.m.Y H:i:s', $last_date);
-    echo '<br>';
-  echo "самая поздняя дата в майами(часовой пояс нью-йорка";
+echo '<br>';
 
-    echo '<br>';
+    date_default_timezone_set('America/New_York');
+    echo date('d.m.Y H:i:s', $last_date);
+      echo '<br>';
+    echo "самая поздняя дата в майами(часовой пояс нью-йорка)";
 
-    echo '<br>';
+echo '<br>';
 
-var_dump ($_SERVER);
-    echo '<br>';
+echo '<br>';
 
-    echo '<br>';
-echo ($_SERVER ["SERVER_ADDR"]);
-    echo '<br>';
-echo ($_SERVER ["SERVER_NAME"]);
-    echo '<br>';
-  date_default_timezone_set('Europe/Moscow');
-echo ($_SERVER ["REQUEST_TIME"]);
-    echo '<br>';
-  echo date ('d.m.Y H:i:s');
+    var_dump ($_SERVER);
+
+echo '<br>';
+
+echo '<br>';
+
+    echo ($_SERVER ["SERVER_ADDR"]);
+
+echo '<br>';
+
+    echo ($_SERVER ["SERVER_NAME"]);
+
+echo '<br>';
+
+    date_default_timezone_set('Europe/Moscow');
+    echo ($_SERVER ["REQUEST_TIME"]);
+
+echo '<br>';
+
+    echo date ('d.m.Y H:i:s');
 ?>
